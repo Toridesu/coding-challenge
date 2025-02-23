197a10207ea4ab2ea89443627ded5c4e63002fc4
Toridesu <160250540+Toridesu@users.noreply.github.com>
2025-02-23 17:32:34 +0900

    チャレンジ詳細画面の作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import { CodeEditor } from './components/CodeEditor';
-import HomeSection from './components/Home';
+import ChallengeDetail from "./components/ChallengeDetail";
+import { CodeEditor } from "./components/CodeEditor";
+import HomeSection from "./components/Home";
+import useChallengeSelection from "./hooks/useChallengeSelection";
 
 export default function App() {
+  const { setCurrentChallengeId, currentChallenge } = useChallengeSelection();
+
   return (
-    <div className='min-h-screen grid grid-cols-2'>
-      <HomeSection onChallengeSelect={(id) => console.log(id)} />
-      <CodeEditor currentChallenge={undefined} onRun={(code) => console.log(code)} />
+    <div className="min-h-screen grid grid-cols-2">
+      {currentChallenge ? (
+        <ChallengeDetail
+          challenge={currentChallenge}
+          results={null}
+          onBack={() => setCurrentChallengeId(null)}
+        />
+      ) : (
+        <HomeSection onChallengeSelect={setCurrentChallengeId} />
+      )}
+      <CodeEditor
+        currentChallenge={currentChallenge}
+        onRun={(code) => console.log(code)}
+      />
     </div>
   );
 }
