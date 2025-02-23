d270ec053781f12f6bb4a41afe08cb14d5d3b184
Toridesu <160250540+Toridesu@users.noreply.github.com>
2025-02-23 18:22:11 +0900

    GitHub Pagesへデプロイする設定を修正

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "/coding-challenge" : "./",
+  base: process.env.GITHUB_PAGES ? '/coding-challenge' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
